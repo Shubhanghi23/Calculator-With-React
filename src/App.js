dcb63c21471d5bc451cd0ed68960799e6e9a679a
Shubhanghi23 <110187973+Shubhanghi23@users.noreply.github.com>
2023-07-24 19:25:23 +0530

    Add files via upload
    Calculator App with React-JS

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Calculator from './components/Calculator';
+import './App.css';
+
+
+
+
+
+
+
+const App =()=> {
+  
+  // var name = "react JS";
+  return (
+    <div>
+    <Calculator/>
+    
+  
+</div>
+  );
+}
+
+
+
+
+
+export default App;
